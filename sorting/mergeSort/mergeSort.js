e6964103c4cd0d6b0f29284d7fba8d2ad1143ac3
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-05 23:06:26 +0700

    Apply prettier

--- sorting/mergeSort/mergeSort.js
+++ sorting/mergeSort/mergeSort.js
@@ -3,11 +3,10 @@
  * How it works:
  * 1. Divide the unsorted list into n sublists, each containing one element (a list of one element is considered sorted).
  * 2. Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining. This will be the sorted list.
- * 
+ *
  * Time complexity: O(n*log(n))
  * Space complexity: O(n)
-*/
-
+ */
 
 function merge(left, right) {
   let mergedArray = [];
