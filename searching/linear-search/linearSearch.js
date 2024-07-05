6d8ff59bcea5ad1f8d80e786629ab6b4b06b25a1
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-05 23:52:38 +0700

    Add "linear search" algorithm in JS

---
+++ searching/linear-search/linearSearch.js
@@ -0,0 +1,19 @@
+/**
+ * Linear search is a search algorithm that finds sequentially checks each element of the array until
+ * a match is found or the whole array has been searched.
+ * How it works:
+ * 1. Check if arr[i] equals to target.
+ * 2. If they are not equal, increase i by 1.
+ * 3. If they are equal, return position of the item (i).
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+
+  return null;
+}
