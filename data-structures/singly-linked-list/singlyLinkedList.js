452baf04dfd5f9152498f43aa0aaa41f2569fa4d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-06 09:13:47 +0700

    Add constant time complexity to description

--- data-structures/singly-linked-list/singlyLinkedList.js
+++ data-structures/singly-linked-list/singlyLinkedList.js
@@ -2,8 +2,9 @@
  * Singly linked list is a linear data structure in which the elements are 
  * not stored in contiguous memory and each element is connected only to 
  * its next element using a pointer.
- * Linked list allows to do insertion and deletion operations in constant time (array does it in O(n)),
- * but it comes with a trade-off of access time complexity being O(n) (array's access time complexity is O(1)).
+ * Linked list allows to do insertion and deletion operations in constant time
+ * of O(1), when array does it in O(n), but it comes with a trade-off of access 
+ * time complexity being O(n) (array's access time complexity is O(1)).
  * 
  * Time complexity:
  * Access: O(n)
