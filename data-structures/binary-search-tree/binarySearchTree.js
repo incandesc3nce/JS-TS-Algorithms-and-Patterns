a8d2ebda6c0fdb864251e8256205b90ee41a2672
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-06 22:51:04 +0700

    Add description to "binary search tree"

--- data-structures/binary-search-tree/binarySearchTree.js
+++ data-structures/binary-search-tree/binarySearchTree.js
@@ -1,3 +1,14 @@
+/**
+ * Binary Search Tree is a data structure used for organizing and storing data in a sorted manner. 
+ * Each node in a Binary Search Tree has at most two children, a left child and a right child, with 
+ * the left child containing values less than the parent node and the right child containing values 
+ * greater than the parent node. This hierarchical structure allows for efficient searching, insertion, 
+ * and deletion operations on the data stored in the tree.
+ * 
+ * Time Complexity: O(log(n))
+ * Space Complexity: O(n)
+ */
+
 class Node {
   constructor(value) {
     this.value = value;
