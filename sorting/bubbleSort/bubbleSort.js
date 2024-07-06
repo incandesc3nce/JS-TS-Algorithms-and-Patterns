298e9412713903be533010ae6ec77c5d7ba28d69
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-06 09:54:11 +0700

    Add "bubble sort" sorting algorithm

---
+++ sorting/bubbleSort/bubbleSort.js
@@ -0,0 +1,22 @@
+/**
+ * Bubble sort is a simple sorting algorithm that repeatedly steps through
+ * the list, compares adjacent elements and swaps them if they are in the 
+ * wrong order. The pass through the list is repeated until the list is
+ * sorted.
+ * 
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
+ */
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
